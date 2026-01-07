77dbe7a35fea6ec478fbd034da55818cfb23b48c
aoDuDu <1049716235@qq.com>
2026-01-07 22:54:30 +0800

    Initial Commit

---
+++ DuMemo/DuMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuMemoApp.swift
+//  DuMemo
+//
+//  Created by 嗷嘟嘟 on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct DuMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
